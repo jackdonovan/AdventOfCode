0b719f983f6c3815cc6ffb074d6969901bb161ae
Jack Donovan <jdonovan@omnitech-inc.com>
2020-12-01 11:50:03 -0600

    adds an actual overload function

--- 2020/Day1/day1.ts
+++ 2020/Day1/day1.ts
@@ -1,15 +1,12 @@
 import fs from "fs";
 
 let nums;
-function addsToValue(num1: number, num2: number, expected: number): number {
+function addsToValue(num1: number, num2: number, expected: number, num3:number=0): number {
   if (num1 + num2 == expected) return num1 * num2;
+  if(num3 != 0 && num1 + num2 + num3 == expected) return num1 * num2 * num3;
   else return 0;
 }
 
-function addsToValueOverride(num1: number, num2: number, num3: number, expected: number): number {
-  if (num1 + num2 + num3 == expected) return num1 * num2 * num3;
-  else return 0;
-}
 
 function calculate() {
   nums = fs.readFileSync("nums.txt", "utf8");
@@ -27,7 +24,7 @@ function calculate() {
   nums.forEach((multiplicand) => {
     multiplierNums.forEach((multiplier) => {
       additionalMultiplierNums.forEach((inner) => {
-        let result = addsToValueOverride(multiplicand, multiplier, inner, 2020);
+        let result = addsToValue(multiplicand, multiplier, 2020, inner);
         if (result != 0) console.log("Possible result? ", result);
       });
     });

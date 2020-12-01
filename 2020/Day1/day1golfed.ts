3b6dca1ac313f9c4e7dc7377432bbf39af3c4463
Jack Donovan <jdonovan@omnitech-inc.com>
2020-12-01 12:05:26 -0600

    ⛳ birdie <- par golf 🏌️‍♂️

--- 2020/Day1/day1golfed.ts
+++ 2020/Day1/day1golfed.ts
@@ -1,4 +1,3 @@
 import fs from "fs";
 let nums=fs.readFileSync("nums.txt", "utf8").split("\n").map(Number);
-nums.forEach(a=>{nums.filter((b)=>(a+b==2020)?console.log(a*b):0)});
-nums.forEach(a=>{nums.forEach(b=> nums.filter(c=>{(a+b+c==2020)?console.log(a*b*c):0}))});
+nums.filter(a=>{nums.filter(b=> nums.filter(c=>{((a+b==2020)?console.log(a*b):0);(a+b+c==2020)?console.log(a*b*c):0}))});
